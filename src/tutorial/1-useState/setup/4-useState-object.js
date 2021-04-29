220d46707b0aca7f1b0c3512a3f8b079eb934dfd
davieu <davisumana@gmail.com>
2021-04-28 21:10:03 -0400

    (4:17:27​) UseState - Object Example ---and--- (4:23:00​) UseState - Multiple State Values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,40 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // using an object as state
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  });
+
+  // Multiple state values. you can also set up as many state values as you want
+  const [name, setName] = useState('john');
+  const [age, setage] = useState('30');
+  const [message, setMessage] = useState("john's message");
+
+  // using the object for setState
+  const changeMessage = () => {
+    setPerson({ ...person, message: 'my new message' });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        Change Message
+      </button>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+    </>
+  );
 };
 
 export default UseStateObject;
