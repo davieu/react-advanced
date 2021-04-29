f44564f4510f56f7ce06e058dbc3c51c24682e26
davieu <davisumana@gmail.com>
2021-04-28 23:21:48 -0400

    (4:47:27​) useEffect - Basics ---through--- (5:01:40​) useEffect - Dependency List

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,37 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log('call useEffect');
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    }
+    // if you add the second argument of [] an empty array then it will only run
+    // the useEffect on the initial render.   }, []); if you add no empty array then it will run after
+    // every render. by adding a dependency (value) it will run everytime (value) is changed or updated
+  }, [value]);
+
+  useEffect(() => {
+    console.log('I will only run on initital render');
+  }, []);
+
+  console.log('render component');
+
+  return (
+    <React.Fragment>
+      <h1>useEffect Basics</h1>
+      <h2>{value}</h2>
+      <button
+        className='btn'
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        Increase
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
