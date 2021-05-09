096d422d5ec3ef0a566c7a710ac69eff7349871e
davieu <davisumana@gmail.com>
2021-05-08 21:04:27 -0400

    react drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { data } from '../../../data';
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <section>
+      <h2>prop drilling use</h2>
+      <List people={people} />
+    </section>
+  );
 };
 
 export default PropDrilling;
